1962e9c22f8e8a2104886ed5f040f7d8e55f64b7
Andy Schetter <andyschetter@gmail.com>
2014-09-25 00:21:37 -0500

    Add linked list and node classes. Create method to add a node at the head

---
+++ linked_list.rb
@@ -0,0 +1,29 @@
+class LinkedList
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def add_head(node)
+    if !@head 
+      @tail = node
+      @head = node
+    else
+      node.next = head
+      @head = node
+    end
+  end
+
+end
+
+class Node
+  attr_reader :value
+  attr_accessor :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
