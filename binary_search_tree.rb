bcd9b8ee8db0ab3d53b8236bc88e02b49ea772ed
Andy Schetter <andyschetter@gmail.com>
2014-09-25 14:57:11 -0500

    Add method to create a root node if the tree is empty

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -5,9 +5,24 @@ class BST
     @root = nil
   end
 
+  def add_node(value)
+    if !@root
+      @root = Node.new(value)
+      return @root
+    end
+    add_helper(@root, value)
+  end
+
+  def add_helper
+  end
+
+
+
+
 end
 
 class Node
+  attr_reader :value
   attr_accessor :left, :right
 
 
