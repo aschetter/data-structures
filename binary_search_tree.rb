2c25dd4781d5348138d4f4eebcb254bc2eaafe17
Andy Schetter <andyschetter@gmail.com>
2014-09-25 15:56:16 -0500

    Add method to find if a value is in the BST

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -32,12 +32,27 @@ class BST
       end
     end
   end
+
+  def find(value)
+    find_helper(@root, value)
+  end
+
+  def find_helper(node, value)
+    if value == node.value
+      return true
+    elsif value < node.value && node.left
+      find_helper(node.left, value)
+    elsif value > node.value && node.right
+      find_helper(node.right, value)
+    else
+      return false
+    end  
+  end
 end
 
 class Node
   attr_reader :value
   attr_accessor :left, :right
-
 
   def initialize(value)
     @value = value
